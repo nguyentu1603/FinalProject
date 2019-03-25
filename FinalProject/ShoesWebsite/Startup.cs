d1cc63076699e76426cd6f946c38f4fb397982b9
Tusturu <tu.lqd.163@gmail.com>
2019-03-25 18:40:47 +0700

    WebsiteLayout

---
+++ FinalProject/ShoesWebsite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ShoesWebsite.Startup))]
+namespace ShoesWebsite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
