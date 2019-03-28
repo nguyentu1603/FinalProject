6799f948a2aa5e3bf716aaa45d36157bfe92b26d
Tusturu <tu.lqd.163@gmail.com>
2019-03-28 10:07:05 +0700

    ChangedLayout

---
+++ FinalProject/ShoesWebsite/Controllers/ManageProductsController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShoesWebsite.Controllers
+{
+    public class ManageProductsController : Controller
+    {
+        //
+        // GET: /ManageProducts/
+        public ActionResult Index() 
+        {
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        public ActionResult Delete()
+        {
+            return View();
+        }
+        public ActionResult Edit()
+        {
+            return View();
+        }
+        public ActionResult DetailProducts()
+        {
+            return View();
+        }
+	}
+}
