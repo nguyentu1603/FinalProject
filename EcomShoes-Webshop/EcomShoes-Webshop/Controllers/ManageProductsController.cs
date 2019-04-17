bdd2d45149e4364156dcd98e8c975dcd9fcb0b68
Tusturu <tu.lqd.163@gmail.com>
2019-04-18 00:38:34 +0700

    Update Layout

---
+++ EcomShoes-Webshop/EcomShoes-Webshop/Controllers/ManageProductsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EcomShoes_Webshop.Controllers
+{
+    public class ManageProductsController : Controller
+    {
+        //
+        // GET: /ManageProducts/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
