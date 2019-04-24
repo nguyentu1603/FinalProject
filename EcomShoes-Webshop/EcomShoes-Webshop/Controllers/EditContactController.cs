f5a958391d901fb09c2c93bc9ac5c6e5699b6c07
dannitran99 <dannitran99@gmail.com>
2019-04-24 17:48:55 +0700

    Update Layout Contact

---
+++ EcomShoes-Webshop/EcomShoes-Webshop/Controllers/EditContactController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EcomShoes_Webshop.Controllers
+{
+    public class EditContactController : Controller
+    {
+        //
+        // GET: /EditContact/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
